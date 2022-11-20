039c32f585dfee40561e5efe8ae43d3dd3d193e7
Storm <lenagreidon@mail.ru>
2022-11-20 13:57:05 +0300

    five

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ module.exports=new Sequelize(
     process.env.DB_PASSWORD,
     {
         dialect:'postgres',
-        host:process.env.DB_HOST,
-        port:process.env.DB_PORT
+        host:process.env.DB_HOST || "localhost",
+        port:process.env.DB_PORT,
     }
 )
