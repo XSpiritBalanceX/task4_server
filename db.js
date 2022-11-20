303f8e0f31a28e8741a41f61cf968625e89b15ad
Storm <lenagreidon@mail.ru>
2022-11-20 14:15:57 +0300

    seven

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const {Sequelize}=require('sequelize');
 
 module.exports=new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.port,
+    process.env.dbname,
+    process.env.dbuser,
+    process.env.dbpassword,
     {
         dialect:'postgres',
-        host:process.env.DB_HOST || "localhost",
-        port:process.env.DB_PORT,
+        host:process.env.dbhost || "localhost",
+        port:process.env.dbport,
     }
 )
