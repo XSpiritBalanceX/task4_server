5d1322bf2828eff082fdc8af350b6b650e62950a
Storm <lenagreidon@mail.ru>
2022-11-20 16:08:33 +0300

    17

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const {Sequelize}=require('sequelize');
 
 module.exports=new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME || 'railway',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || 'pfMYfubSfOdqlXfWZAO7',
     {
         dialect:'postgres',
-        host:process.env.DB_HOST || "localhost",
-        port:process.env.DB_PORT,
+        host:process.env.DB_HOST || "containers-us-west-137.railway.app",
+        port:process.env.DB_PORT || 7845,
     }
 )
